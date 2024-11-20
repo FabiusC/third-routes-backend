433390fd32875af9036080d46973d89dcab27493
FabiusC <fabioandrescar@gmail.com>
2024-11-20 16:38:50 -0500

    Update Database info

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 // Configuración de la base de datos (actualizar los valores con los de tu instancia RDS)
 const pool = new Pool({
   user: "postgres",
-  host: "third-parties-lm.cluster-c90iyucmovrc.us-east-2.rds.amazonaws.com",
+  host: "thirds-lm.cdkukm4ik3ba.us-east-1.rds.amazonaws.com",
   database: "postgres",
   password: "Cuenta?AWS5%",
   port: 5432,
