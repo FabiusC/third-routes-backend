f678e9b58e7f76a9bc3a68b63b93b41ca250b999
FabiusC <fabioandrescar@gmail.com>
2024-11-21 13:23:54 -0500

    update port

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,9 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import routes from "./routes/routes";
-import https from "https";
-import fs from "fs";
 
 const app = express();
-const PORT = 8080;
-
-// Cargar los certificados SSL
-const options = {
-  key: fs.readFileSync("server.key"), // Ruta al archivo de clave privada
-  cert: fs.readFileSync("server.cert"), // Ruta al archivo de certificado
-};
+const PORT = 3000;
 
 // Middleware
 app.use(cors());
@@ -21,7 +13,7 @@ app.use(bodyParser.json());
 // Rutas
 app.use("/api", routes);
 
-// Crear el servidor HTTPS
-https.createServer(options, app).listen(PORT, () => {
+// Inicio del servidor
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en https://localhost:${PORT}`);
 });
